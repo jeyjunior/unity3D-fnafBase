e57954e975a9cfd63555fb926578807d67628a03
jeyjunior <jeyjuniordg@gmail.com>
2021-10-23 09:10:27 -0300

    Interacao com cameras

---
+++ Assets/Scripts/CamControll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CamControll : MonoBehaviour
+{
+
+    public Text textMensagem;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.name == "PC")
+        {
+            textMensagem.text = "Press Enter to view the cams";
+        }
+    }
+
+}
